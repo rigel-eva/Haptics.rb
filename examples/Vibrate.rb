13559eac0e92e75cb3d9fbd430fc6754177c4637
Nora Maguire <eva@rigel.moe>
2018-02-15 00:20:46 -0600

    the README is useful now!
    also ... adjsted vibrate example to add more ... patterns to play with 0w0

--- examples/Vibrate.rb
+++ examples/Vibrate.rb
@@ -1,17 +1,56 @@
 require "buttplugrb"
 LOCATION="wss://localhost:12345/buttplug"           #where our server is located, more than likely going to be on localhost:12345/buttplug
+def pattern1(controller)
+    controller.vibrateAll(0.25)
+    sleep (3)
+    controller.vibrateAll(1)
+    sleep (1)  
+    controller.vibrateAll(0.3)
+    sleep 2
+    controller.vibrateAll 1
+    sleep 2
+    controller.vibrateAll 0.6
+    sleep 5
+    controller.vibrateAll 0.5
+    sleep 0.5
+    controller.vibrateAll 1
+    sleep 20
+end
+def pulsePattern(controller)
+    controller.vibrateAll(0)
+    sleep 0.5
+    controller.vibrateAll(1)
+    sleep 0.5
+end
+def lickPattern(controller)
+    controller.vibrateAll 0
+    sleep 0.25
+    controller.vibrateAll 0.5
+    sleep 0.25
+end
+def randomPattern(controller)
+    controller.vibrateAll(rand(0.0..1.0))   #Set our vibration somewhere in the range of valid numbers
+    sleep(rand(5..60))                     #and sleep for a random period of time
+end
 client=Buttplug::Client.new(LOCATION)               #initalizing our client 
 sleep 0.1                                           #Giving our client a moment to wake up
 client.startScanning();                             #Telling the server to start scanning for new devices, ble or otherwise
 sleep 1                                             #Giving it a moment to find our device
-devices=client.listDevices()                        #Grabing our device list
+devices=[]
+while devices==[] do 
+  devices=client.listDevices()                        #Grabing our device list
+  sleep 1
+end
 client.stopScanning()                               #No sense in tying up the server ... not just yet~
 controller=Buttplug::Device.new(client,devices[0])  #and generating a new device from the first client on the list
 #ok we are gonna make a few assumptions here ... primarily that you are using an xbox controller
 if(controller.methods.include? :vibrateAll)         #Ok due to metaprogramming bs we have to check to see if the device in question supports vibration
-  (0..20).each{|i|                          #So we are going to do a shortish random curve
-    controller.vibrateAll(rand(0.0..1.0))   #Set our vibration somewhere in the range of valid numbers
-    sleep(rand(0.5..2))                     #and sleep for a random period of time
+  (0..20000).each{|i|
+    (0..5).each{|j|
+    lickPattern(controller)
+    }
+    controller.vibrateAll 1
+    sleep 10
   }
   controller.vibrateAll(0.0)                #and shutting off the lights~
 end
--- gem/README.md
+++ gem/README.md
@@ -1,8 +1,6 @@
-# Buttplugrb
+# Buttplugrb - Shove Ruby up your ass!
 
-Welcome to your new gem! In this directory, you'll find the files you need to be able to package up your Ruby library into a gem. Put your Ruby code in the file `lib/buttplugrb`. To experiment with that code, run `bin/console` for an interactive prompt.
-
-TODO: Delete this and the text above, and describe your gem
+Welp ... You have wandered into probably one of the silliest things I set up ... This is a gem to make your ruby app act as a client for a buttplug.io server ... 
 
 ## Installation
 
@@ -22,22 +20,15 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+You can get client up and running like this:
 
-## Development
+```ruby
+require "buttplugrb"
+client=Buttplug::Client.new("wss://localhost:12345/buttplug")
+``` 
 
-After checking out the repo, run `bin/setup` to install dependencies. You can also run `bin/console` for an interactive prompt that will allow you to experiment.
-
-To install this gem onto your local machine, run `bundle exec rake install`. To release a new version, update the version number in `version.rb`, and then run `bundle exec rake release`, which will create a git tag for the version, push git commits and tags, and push the `.gem` file to [rubygems.org](https://rubygems.org).
-
-## Contributing
-
-Bug reports and pull requests are welcome on GitHub at https://github.com/[USERNAME]/buttplugrb. This project is intended to be a safe, welcoming space for collaboration, and contributors are expected to adhere to the [Contributor Covenant](http://contributor-covenant.org) code of conduct.
+You can view some additional examples in the Examples Folder!
 
 ## License
 
 The gem is available as open source under the terms of the [MIT License](https://opensource.org/licenses/MIT).
-
-## Code of Conduct
-
-Everyone interacting in the Buttplugrb project’s codebases, issue trackers, chat rooms and mailing lists is expected to follow the [code of conduct](https://github.com/[USERNAME]/buttplugrb/blob/master/CODE_OF_CONDUCT.md).
